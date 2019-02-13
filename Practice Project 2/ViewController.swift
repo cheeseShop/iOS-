ba1c734710974cc1f22f4dabf22bc299c8c7cf57
Simon Ludwig <simonludwig@Simons-MacBook-Pro.local>
2019-02-13 17:02:14 -0500

    Initial Commit

---
+++ Practice Project 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice Project 2
+//
+//  Created by Simon Ludwig on 2/13/19.
+//  Copyright © 2019 Simon Ludwig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
